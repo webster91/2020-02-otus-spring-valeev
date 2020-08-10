131b95f8c35adda2b8c774876bf9ad30997d2d51
Arthur <webster_91@inbox.ru>
2020-08-10 19:27:50 +0300

    homework-18 Добавил hystrix

--- homework18/src/main/java/ru/otus/valeev/service/impl/BookServiceImpl.java
+++ homework18/src/main/java/ru/otus/valeev/service/impl/BookServiceImpl.java
@@ -1,6 +1,8 @@
 package ru.otus.valeev.service.impl;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.otus.valeev.dao.BookDao;
@@ -12,26 +14,37 @@ import ru.otus.valeev.service.AuthorService;
 import ru.otus.valeev.service.BookService;
 import ru.otus.valeev.service.GenreService;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class BookServiceImpl implements BookService {
     private final BookDao bookDao;
     private final AuthorService authorService;
     private final GenreService genreService;
+    private final Random random = new Random();
 
     @Override
+    @HystrixCommand(commandKey = "findAll", fallbackMethod = "fallbackGetBooks")
     public List<Book> findAll() {
+        boolean rnd = random.nextBoolean();
+        if (rnd) {
+            throw new RuntimeException("Не повезло");
+        }
         return bookDao.findAll();
     }
 
     @Override
+    @HystrixCommand(commandKey = "findByName", fallbackMethod = "fallbackGetBook")
     public Book findByName(String name) {
         return bookDao.findByName(name);
     }
 
     @Override
+    @HystrixCommand(commandKey = "findById", fallbackMethod = "fallbackGetBook")
     public Book findById(String bookId) {
         return bookDao.findById(bookId).orElse(null);
     }
@@ -78,9 +91,31 @@ public class BookServiceImpl implements BookService {
 
     @Override
     @Transactional(readOnly = true)
+    @HystrixCommand(commandKey = "findCommentsBy", fallbackMethod = "fallbackGetBook")
     public List<Comment> findCommentsByBookId(String bookId) {
         return bookDao.findById(bookId)
                 .map(Book::getComments)
                 .orElse(null);
+    }
+
+    private Book fallbackGetBook(String id) {
+        Genre genre = new Genre();
+        genre.setId(id);
+        genre.setName("Дорама");
+
+        Author author = new Author();
+        author.setId("0");
+        author.setName("Сын маминой подруги");
+
+        Book book = new Book();
+        book.setId("0");
+        book.setName("Книга которую ты не заказывал");
+        book.setGenre(genre);
+        book.setAuthor(author);
+        return book;
+    }
+
+    private List<Book> fallbackGetBooks() {
+        return Collections.singletonList(fallbackGetBook("0"));
     }
 }
